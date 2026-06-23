4aaf60274abd06fb64f0267e56423263a22d73c5
Petya Naydenova Ferreira <pn@abtion.com>
2026-06-23 12:32:26 +0200

    remove empty save.js files

--- wp-content/plugins/vandrekalender-events/blocks/event-calendar/index.js
+++ wp-content/plugins/vandrekalender-events/blocks/event-calendar/index.js
@@ -7,11 +7,10 @@ import { registerBlockType } from '@wordpress/blocks';
  * Internal dependencies.
  */
 import Edit from './edit';
-import save from './save';
 import metadata from './block.json';
 import './style.scss';
 
 registerBlockType( metadata.name, {
 	edit: Edit,
-	save,
+	save: () => null,
 } );
--- wp-content/plugins/vandrekalender-events/blocks/event-calendar/save.js
+++
@@ -1,6 +0,0 @@
-/**
- * Dynamic block — rendered server-side via render.php.
- */
-export default function save() {
-	return null;
-}
