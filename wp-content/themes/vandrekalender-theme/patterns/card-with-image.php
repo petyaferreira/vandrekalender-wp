9169ff000e9df2019a04be8687d58518fac9330a
Petya Naydenova Ferreira <pn@abtion.com>
2026-07-21 12:43:58 +0300

    add a card with image template for slides

---
+++ wp-content/themes/vandrekalender-theme/patterns/card-with-image.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: Card with Image
+ * Slug: vandrekalender-theme/card-with-image
+ * Categories: featured
+ * Description: A card with an image, heading, paragraph, and author information. Drop it inside a slider slide or use it on its own anywhere on the page.
+ *
+ * @package vandrekalender-theme
+ */
+
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1348,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":{"topLeft":"1rem","topRight":"1rem"}}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="http://localhost:8080/wp-content/uploads/2026/07/pexels-suju-30194750-1024x683.jpg" alt="" class="wp-image-1348" style="border-top-left-radius:1rem;border-top-right-radius:1rem"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"top":"var:preset|spacing|medium-clamp","bottom":"var:preset|spacing|medium-clamp","left":"var:preset|spacing|medium-clamp","right":"var:preset|spacing|medium-clamp"}},"border":{"radius":{"bottomLeft":"1rem","bottomRight":"1rem"}}},"backgroundColor":"cream","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-cream-background-color has-background" style="border-bottom-left-radius:1rem;border-bottom-right-radius:1rem;padding-top:var(--wp--preset--spacing--medium-clamp);padding-right:var(--wp--preset--spacing--medium-clamp);padding-bottom:var(--wp--preset--spacing--medium-clamp);padding-left:var(--wp--preset--spacing--medium-clamp)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Tour around Amager</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>This tour is free and for everybody who enjoys walking and wants to be in nature</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"style":{"typography":{"textAlign":"right","fontStyle":"italic","fontWeight":"400"}}} -->
+<p class="has-text-align-right" style="font-style:italic;font-weight:400">From Petya</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
